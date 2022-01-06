8c0ce9818496a0ecd6ed5315f186f58b97453816
Yu Ra Kim <yurakim.designs@gmail.com>
2022-01-06 11:58:12 -0500

    Phase 1 complete

---
+++ authenticate-me/backend/routes/api/index.js
@@ -0,0 +1,13 @@
+// -------------------- IMPORTS -------------------- //
+
+const router = require('express').Router();
+
+// -------------------- ROUTES -------------------- //
+
+router.post('/test', function(req, res) {
+    res.json({ requestBody: req.body });
+});
+
+// -------------------- EXPORTS -------------------- //
+
+module.exports = router;
--- authenticate-me/backend/routes/index.js
+++ authenticate-me/backend/routes/index.js
@@ -1,9 +1,23 @@
+// -------------------- IMPORTS -------------------- //
+
 const express = require('express');
 const router = express.Router();
+
+const apiRouter = require('./api');
+
+// -------------------- router.use -------------------- //
+
+router.use('/api', apiRouter);
+
+
+
+// -------------------- ROUTES -------------------- //
 
 router.get('/hello/world', function(req, res) {
     res.cookie('XSRF-TOKEN', req.csrfToken());
     res.send('Hello World!');
 });
+
+// -------------------- EXPORTS -------------------- //
 
 module.exports = router;
