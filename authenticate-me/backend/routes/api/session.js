41c8922599641e8be487a878ec9245d16df6609f
Yu Ra Kim <yurakim.designs@gmail.com>
2022-01-06 13:59:32 -0500

    Part 4 complete

--- authenticate-me/backend/routes/api/session.js
+++ authenticate-me/backend/routes/api/session.js
@@ -41,6 +41,19 @@ router.delete(
     }
 );
 
+router.get(
+    '/',
+    restoreUser,
+    (req, res) => {
+        const { user } = req;
+        if (user) {
+            return res.json({
+                user: user.toSafeObject()
+            });
+        } else return res.json({});
+    }
+);
+
 // -------------------- EXPORTS -------------------- //
 
 module.exports = router;
