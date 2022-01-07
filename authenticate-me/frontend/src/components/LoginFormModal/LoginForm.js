25927218686840b22a8d1561af617af6c84907b6
Yu Ra Kim <yurakim.designs@gmail.com>
2022-01-07 15:40:43 -0500

    Frontend Login Modal

--- authenticate-me/frontend/src/components/LoginFormModal/LoginForm.js
+++ authenticate-me/frontend/src/components/LoginFormModal/LoginForm.js
@@ -1,33 +1,31 @@
-import React, { useState } from 'react';
-import * as sessionActions from '../../store/session';
-import { useDispatch, useSelector } from 'react-redux';
-import { Redirect } from 'react-router-dom';
+import React, { useState } from "react";
+import * as sessionActions from "../../store/session";
+import { useDispatch } from "react-redux";
 
-import './LoginForm.css';
-
-function LoginFormPage() {
+function LoginForm() {
     const dispatch = useDispatch();
-    const sessionUser = useSelector(state => state.session.user);
-    const [credential, setCredential] = useState('');
-    const [password, setPassword] = useState('');
+    const [credential, setCredential] = useState("");
+    const [password, setPassword] = useState("");
     const [errors, setErrors] = useState([]);
-
-    if (sessionUser) return (
-        <Redirect to="/" />
-    );
 
     const handleSubmit = (e) => {
         e.preventDefault();
         setErrors([]);
-        return dispatch(sessionActions.login({ credential, password }))
-            .catch(async (res) => {
+        return dispatch(sessionActions.login({ credential, password })).catch(
+            async (res) => {
                 const data = await res.json();
                 if (data && data.errors) setErrors(data.errors);
-            });
-    }
+            }
+        );
+    };
 
     return (
         <form onSubmit={handleSubmit}>
+            <ul>
+                {errors.map((error, idx) => (
+                    <li key={idx}>{error}</li>
+                ))}
+            </ul>
             <label>
                 Username or Email
                 <input
@@ -47,11 +45,8 @@ function LoginFormPage() {
                 />
             </label>
             <button type="submit">Log In</button>
-            <ul className="errors">
-                {errors.map((error, idx) => <li key={idx}>{error}</li>)}
-            </ul>
         </form>
     );
 }
 
-export default LoginFormPage;
+export default LoginForm;
