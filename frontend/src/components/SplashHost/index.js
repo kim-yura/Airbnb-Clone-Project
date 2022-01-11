327bb6c74e6a13df6425c577cb6190be10ff521a
Yu Ra Kim <yurakim.designs@gmail.com>
2022-01-11 17:44:46 -0500

    Splash Host redirect to creat form

--- frontend/src/components/SplashHost/index.js
+++ frontend/src/components/SplashHost/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 import './SplashHost.css';
 
 function SplashHost() {
     return (
-        <div className="host-container">
-            <div className="host-text">
-                Become a Host.
+        <Link to="/spots/new">
+            <div className="host-container">
+                <div className="host-text">
+                    Become a Host.
+                </div>
+                <div id="splash-host">Image 01</div>
             </div>
-            <div id="splash-host">Image 01</div>
-        </div>
+        </Link>
     )
 };
 
