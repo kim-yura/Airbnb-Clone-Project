709c03131d45598f2d2ac434a5f2d467fe936983
Yu Ra Kim <yurakim.designs@gmail.com>
2022-01-13 11:25:00 -0500

    CSS styling for confirm delete? modal

--- frontend/src/components/SpotDeleteConfirmationModal/ConfirmDelete.js
+++ frontend/src/components/SpotDeleteConfirmationModal/ConfirmDelete.js
@@ -1,8 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
 
-import {deleteSpot} from "../../store/spotReducer";
+import { deleteSpot } from "../../store/spotReducer";
+
+import "./ConfirmDelete.css";
 
 function ConfirmDelete() {
     const dispatch = useDispatch();
@@ -15,13 +17,17 @@ function ConfirmDelete() {
     const spotObj = Object.values(spot)[0];
 
     return (
-        <a
-            onClick={() => {
-                dispatch(deleteSpot(spotObj.id));
-                history.push("/spots-delete-confirmation");
-            }}>
-            Confirm Delete
-        </a>
+        <div className="confirm-delete-modal">
+            <h3>Are you sure you want to delete this Spot?</h3>
+            <a
+                className="confirm-delete-button"
+                onClick={() => {
+                    dispatch(deleteSpot(spotObj.id));
+                    history.push("/spots-delete-confirmation");
+                }}>
+                Confirm Delete
+            </a>
+        </div>
     )
 };
 
