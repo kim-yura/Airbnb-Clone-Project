5a0ed762778a2577d1f910d528c4e5fce4a858d1
Yu Ra Kim <yurakim.designs@gmail.com>
2022-01-13 11:28:25 -0500

    Reroute button added to delete confirmed page

--- frontend/src/components/SpotDeleteConfirmationModal/DeleteConfirmed.js
+++ frontend/src/components/SpotDeleteConfirmationModal/DeleteConfirmed.js
@@ -5,6 +5,11 @@ function DeleteConfirmed() {
         <div className="delete-confirmation">
             <h2 className="delete-success-text">Spot Successfully Deleted!</h2>
             <i className="fas fa-trash-alt"></i>
+            <a
+                className="confirm-delete-button back-to-home-button"
+                href="/">
+                Back to Home
+            </a>
         </div>
     )
 };
