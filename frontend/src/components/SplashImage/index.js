219e9c055bd2f392b7655c8f236140959b4915dd
Yu Ra Kim <yurakim.designs@gmail.com>
2022-03-02 15:41:47 -0500

    CSS Refactoring of SplashImage and SpotDisplay

--- frontend/src/components/SplashImage/index.js
+++ frontend/src/components/SplashImage/index.js
@@ -3,28 +3,18 @@ import './SplashImage.css';
 
 function SplashImage() {
     return (
-        <div className="splash-container">
-            <ul className="splash-images">
-                <li>
-                    <div id="splash-01">Image 01</div>
-                </li>
-                <li>
-                    <div id="splash-02">Image 02</div>
-                </li>
-                <li>
-                    <div id="splash-03">Image 03</div>
-                </li>
-                <li>
-                    <div id="splash-04">Image 04</div>
-                </li>
-                <li>
-                    <div id="splash-05">Image 05</div>
-                </li>
-            </ul>
+        <>
+            <div className="splash-container">
+                <div id="splash-01" />
+                <div id="splash-02" />
+                <div id="splash-03" />
+                <div id="splash-04" />
+                <div id="splash-05" />
+            </div >
             <div className="splash-text">
                 Travel more, with less.
             </div>
-        </div>
+        </>
     )
 }
 
