d8c2d735f814b1bb84ff31b817e3dfea7a2285f7
Yu Ra Kim <yurakim.designs@gmail.com>
2022-03-15 11:29:43 -0400

    Bookings API router

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -9,3 +9,41 @@ const { response } = require('express');
 const { Booking, Spot, User } = db;
 
 const router = express.Router();
+
+router.post('/new', asyncHandler(async (req, res) => {
+    const newBooking = req.body;
+
+    const {
+        userId,
+        spotId,
+        startDate,
+        endDate,
+        reasonForStay
+    } = req.body;
+
+    await Booking.create(newBooking);
+    return res.json({ newBooking });
+}));
+
+router.get('/:id(\\d+)/edit', asyncHandler(async (req, res) => {
+    const bookingId = req.params.id;
+    const booking = await Booking.findByPk(bookingId);
+    return res.json(booking);
+}));
+
+router.put('/:id(\\d+)', asyncHandler(async (req, res) => {
+    const bookingId = req.params.id;
+    const oldBooking = await Booking.findByPk(bookingId);
+    const updatedBooking = await oldSpot.update(req.body);
+    return res.json(updatedBooking);
+}));
+
+router.delete('/:id(\\d+)', asyncHandler(async (req, res) => {
+    const bookingId = req.params.id;
+    const booking = await Booking.findByPk(bookingId);
+    await booking.destroy();
+    return res.json({ booking });
+}));
+
+
+module.exports = router;
